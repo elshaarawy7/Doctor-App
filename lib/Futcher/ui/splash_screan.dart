1734db495168e20886989120a7872a156e56ca06
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-09-19 10:55:01 +0300

    create spalsh_screan

---
+++ lib/Futcher/ui/splash_screan.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SplashScrean extends StatelessWidget {
+  const SplashScrean({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
